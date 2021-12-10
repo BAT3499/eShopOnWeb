5dfa788362f1d78da93d906db06e6a85e9520917
Betsegaw Amare <bamare@css.edu>
2021-12-09 18:26:22 -0600

    Added a new attribute : color to catalog items

--- src/ApplicationCore/Entities/CatalogItem.cs
+++ src/ApplicationCore/Entities/CatalogItem.cs
@@ -10,27 +10,31 @@ public class CatalogItem : BaseEntity, IAggregateRoot
     public string Description { get; private set; }
     public decimal Price { get; private set; }
     public string PictureUri { get; private set; }
+    public string Color { get; private set; }
     public int CatalogTypeId { get; private set; }
     public CatalogType CatalogType { get; private set; }
     public int CatalogBrandId { get; private set; }
     public CatalogBrand CatalogBrand { get; private set; }
 
     public CatalogItem(int catalogTypeId,
-        int catalogBrandId,
+        int catalogBrandId, int catalogItemId,
         string description,
         string name,
         decimal price,
-        string pictureUri)
+        string pictureUri, 
+        string color)
     {
         CatalogTypeId = catalogTypeId;
         CatalogBrandId = catalogBrandId;
+        CatalogItemId = catalogItemId;
         Description = description;
         Name = name;
         Price = price;
         PictureUri = pictureUri;
+        color = color;
     }
 
-    public void UpdateDetails(string name, string description, decimal price)
+    public void UpdateDetails(string name, string description, decimal price, string color)
     {
         Guard.Against.NullOrEmpty(name, nameof(name));
         Guard.Against.NullOrEmpty(description, nameof(description));
@@ -39,6 +43,7 @@ public class CatalogItem : BaseEntity, IAggregateRoot
         Name = name;
         Description = description;
         Price = price;
+        Color = color;
     }
 
     public void UpdateBrand(int catalogBrandId)
@@ -62,4 +67,5 @@ public class CatalogItem : BaseEntity, IAggregateRoot
         }
         PictureUri = $"images\\products\\{pictureName}?{new DateTime().Ticks}";
     }
+
 }
